7e9f2640b54a6d6e9c83f812e188e73f2771b528
Renan Caldas <renan@renancaldas.dev.br>
2025-05-10 10:30:43 -0300

    [feature/create_tests] teste de autenticacao

--- database/factories/Users/UserFactory.php
+++ database/factories/Users/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Users\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -11,6 +12,9 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+
+    protected $model = User::class;
+
     /**
      * The current password being used by the factory.
      */
@@ -24,10 +28,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'user_name' => $this->faker->unique()->userName(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'profile_picture' => null,
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('password'), // ou Hash::make(...)
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +44,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
